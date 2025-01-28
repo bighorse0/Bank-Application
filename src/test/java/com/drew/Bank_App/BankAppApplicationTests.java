140ccc4e0ca24f2b3b821932b6d5e24050e7a194
drewstorey <drewtstorey@gmail.com>
2025-01-27 19:36:20 -0500

    Initial

---
+++ src/test/java/com/drew/Bank_App/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drew.Bank_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
