730c7a9b4969047502c32156f6acd6c48f9f6365
drewstorey <drewtstorey@gmail.com>
2025-01-29 16:51:51 -0500

    Credit and debit functionality

---
+++ src/main/java/com/drew/Bank_App/dto/AccountInquiryRequest.java
@@ -0,0 +1,16 @@
+package com.drew.Bank_App.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountInquiryRequest {
+    private String accountNumber;
+
+}
--- src/main/java/com/drew/Bank_App/utils/AccountUtils.java
+++ src/main/java/com/drew/Bank_App/utils/AccountUtils.java
@@ -8,6 +8,14 @@ public class AccountUtils {
     public static final String ACC_CREATED_MESSAGE = "Account created";
     public static final String ACC_EXISTS = "1";
     public static final String ACC_EXISTS_MESSAGE = "An account with this email already exists";
+    public static final String ACC_NOT_EXISTS = "2";
+    public static final String ACC_NOT_EXISTS_MESSAGE = "An account with this email does not exist";
+    public static final String ACC_HAS_BEEN_CREDITED = "3";
+    public static final String ACC_HAS_BEEN_CREDITED_MESSAGE = "Account has been credited successfully";
+    public static final String ACC_HAS_INSUFFICIENT_FUNDS = "4";
+    public static final String ACC_HAS_INSUFFICIENT_FUNDS_MESSAGE = "Account has been insufficient funds";
+    public static final String ACC_HAS_BEEN_DEBITED = "5";
+    public static final String ACC_HAS_BEEN_DEBITED_MESSAGE = "Account has been debited";
 
     public static String generateAccountNumber() {
         /**
